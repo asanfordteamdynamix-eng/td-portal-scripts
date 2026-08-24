904fe328ce7b6c867ff9787d715030a3588f941b
asanfordteamdynamix-eng <asanford.teamdynamix@gmail.com>
2026-08-24 14:59:12 -0400

    Update hide-services.js

--- hide-services.js
+++ hide-services.js
@@ -1,1 +1,15 @@
-alert("GitHub JavaScript loaded!");
+(function () {
+    function hideServices() {
+        var services = document.getElementById("divTDRequests");
+
+        if (services) {
+            services.style.display = "none";
+        }
+    }
+
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", hideServices);
+    } else {
+        hideServices();
+    }
+})();
